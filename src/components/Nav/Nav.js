ce99907ffb838dfb627c60341ec1e2b17f27eca6
choyondas <choyondas08@gmail.com>
2022-06-01 00:59:37 +0800

    floating navbar done

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import './Nav.css'
 import { AiOutlineHome } from 'react-icons/ai';
 import { BiUser } from 'react-icons/bi';
 import { SiKnowledgebase } from 'react-icons/si';
 import { MdOutlineDesignServices } from 'react-icons/md';
 import { RiContactsBookLine } from 'react-icons/ri';
+
 const Nav = () => {
     return (
         <nav>
-            <a href="#"><AiOutlineHome/></a>
+            <a href="#" className="active"><AiOutlineHome/></a>
             <a href="#about"><BiUser/></a>
             <a href="#experience"><SiKnowledgebase/></a>
             <a href="#services"><MdOutlineDesignServices/></a>
