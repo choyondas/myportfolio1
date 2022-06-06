dfa11de9c6314f5d3fa451d85533232fcd84c6d8
choyondas <choyondas08@gmail.com>
2022-06-06 16:45:53 +0800

    redirects

--- src/components/Testimonial/Testimonial.js
+++ src/components/Testimonial/Testimonial.js
@@ -5,34 +5,44 @@ import avatar2 from "../../assets/avatar-2.jpg"
 import avatar3 from "../../assets/avatar-3.jpg"
 import avatar4 from "../../assets/avatar-4.jpg"
 import avatar5 from "../../assets/avatar-5.jpg"
+// import Swiper core and required modules
+import { Pagination,EffectCoverflow  } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import "swiper/css/effect-coverflow";
+import 'swiper/css/pagination';
 
 
 const data = [
     
     {
-        avatar: avatar1,
-        name: 'Max',
-        review: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto, dolores?',
+        avatar: avatar5,
+        
+        name: 'Asal Mshk',
+        review: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati officia asperiores, nulla officiis optio amet molestiae natus vero accusantium corporis unde, nemo ducimus, corrupti quis? Laudantium delectus dolor nostrum tempore!',
     },
     {
         avatar: avatar2,
         name: 'Shatta Wale',
-        review: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto, dolores?',
+        review: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati officia asperiores, nulla officiis optio amet molestiae natus vero accusantium corporis unde, nemo ducimus, corrupti quis? Laudantium delectus dolor nostrum tempore!',
     },
     {
         avatar: avatar3,
         name: 'David Mill',
-        review: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto, dolores?',
+        review: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati officia asperiores, nulla officiis optio amet molestiae natus vero accusantium corporis unde, nemo ducimus, corrupti quis? Laudantium delectus dolor nostrum tempore!',
     },
     {
         avatar: avatar4,
-        name: 'Tom',
-        review: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto, dolores?',
+        name: 'Magnet',
+        review: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati officia asperiores, nulla officiis optio amet molestiae natus vero accusantium corporis unde, nemo ducimus, corrupti quis? Laudantium delectus dolor nostrum tempore!',
     },
     {
-        avatar: avatar5,
-        name: 'Noushin',
-        review: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto, dolores?',
+        avatar: avatar1,
+        name: 'Max',
+        review: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati officia asperiores, nulla officiis optio amet molestiae natus vero accusantium corporis unde, nemo ducimus, corrupti quis? Laudantium delectus dolor nostrum tempore!',
     },
 ]
 
@@ -43,11 +53,29 @@ const Testimonial = () => {
             <h5>Review From Clients</h5>
             <h2>Testimonial</h2>
 
-            {
-                data.map(({ avatar, name, review }) => {
+           
+            <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+         className="container testimonial-container"
+             
+            >
+                
+                 {
+                data.map(({ avatar, name, review }, index) => {
                     return (
-                         <div className="container testimonial-container">
-                <article className="testimonial">
+                <SwiperSlide key={index} className="testimonial">
                     <div className="client_avatar">
                         <img className='avatar-image' src={avatar} alt="" />
                     </div>
@@ -55,15 +83,15 @@ const Testimonial = () => {
                         <small className="client_review">
                             {review}
                         </small>
-                </article>
+                </SwiperSlide>
                 
-            </div>
+            
                   )
 
 
                 })
             }
-
+</Swiper>
            
         </section>
     );
