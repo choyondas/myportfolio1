112bf6cc7eff15a7f6064eaaadef263a4b55d9db
choyondas <choyondas08@gmail.com>
2022-06-19 17:48:42 +0800

    added new resume

--- src/components/Header/CTA.js
+++ src/components/Header/CTA.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import CV from '../../assets/CHOYON DAS RESUME.pdf';
+import CV from '../../assets/Choyon Das CV.pdf';
 const CTA = () => {
     return (
         <div className="cta">
